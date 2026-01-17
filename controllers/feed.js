cd0b39288f754802d03c6b464d5c7b02b9c2a322
Vijay0908-lab <chauhanvijay0908@gmail.com>
2026-01-18 02:48:46 +0530

    basic understanding of REST_API is done

---
+++ controllers/feed.js
@@ -0,0 +1,14 @@
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ title: "first title ", content: "this is content" }],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  res.status(201).json({
+    message: "Post created successfully",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};

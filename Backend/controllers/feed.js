0c6564b6bbe787d81a24a45217bca026333560d8
Vijay0908-lab <chauhanvijay0908@gmail.com>
2026-01-22 02:21:02 +0530

    backend and front-end is initiall url are connected

--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: "1",
+        creator: {
+          name: "vijay",
+        },
+        createdAt: new Date(),
+        title: "First Post",
+        content: "This is the second!",
+        imageUrl: "/images/coconut.jpg",
+      },
+    ],
   });
 };
 
@@ -9,7 +20,15 @@ exports.createPost = (req, res, next) => {
   const content = req.body.content;
   // Create post in db
   res.status(201).json({
-    message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: "vijay",
+      },
+      createdAt: new Date(),
+    },
   });
 };

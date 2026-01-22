531e133dae2f9bab9ce830e8bd4e122426851fd1
Vijay0908-lab <chauhanvijay0908@gmail.com>
2026-01-23 01:22:03 +0530

    databse connected

--- Backend/app.js
+++ Backend/app.js
@@ -1,20 +1,40 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const feedRoutes = require('./routes/feed');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const feedRoutes = require("./routes/feed");
+const path = require("path");
 const app = express();
 
 // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
 app.use(bodyParser.json()); // application/json
 
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE",
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
 });
 
-app.use('/feed', feedRoutes);
+app.use("/feed", feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+  console.log(err);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+mongoose
+  .connect(
+    "mongodb+srv://Vijay4944:Vijay4944@cluster0.9u1hgcw.mongodb.net/messages?appName=Cluster0",
+  )
+  .then((result) => {
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- Backend/controllers/feed.js
+++ Backend/controllers/feed.js
@@ -1,3 +1,7 @@
+//const { check } = require("express-validator");
+
+const { validationResult } = require("express-validator");
+const Post = require("../model/post");
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -16,19 +20,37 @@ exports.getPosts = (req, res, next) => {
 };
 
 exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+
+  // Create post in db
+
+  if (!errors.isEmpty()) {
+    const error = new Error("validation failed entered data is incorrect");
+    error.statusCode = 422;
+    throw error;
+  }
   const title = req.body.title;
   const content = req.body.content;
-  // Create post in db
-  res.status(201).json({
-    message: "Post created successfully!",
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: {
-        name: "vijay",
-      },
-      createdAt: new Date(),
-    },
+
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/coconut.jpeg",
+    creator: { name: "Vijay" },
   });
+  post
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "Post created successfully!",
+        post: result,
+      });
+    })
+    .catch((err) => {
+      if (err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };

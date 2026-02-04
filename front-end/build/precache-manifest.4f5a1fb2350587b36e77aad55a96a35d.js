8405c500594312aeed78b57fcd3bc63e3681c972
Vijay0908-lab <chauhanvijay0908@gmail.com>
2026-02-05 00:59:42 +0530

    small changes in front-end

---
+++ front-end/build/precache-manifest.4f5a1fb2350587b36e77aad55a96a35d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c3c3002b68744b8bfd",
+    "url": "/static/css/main.ef3e6e15.chunk.css"
+  },
+  {
+    "revision": "20c3c3002b68744b8bfd",
+    "url": "/static/js/main.20c3c300.chunk.js"
+  },
+  {
+    "revision": "408e0e3ab30942cdb935",
+    "url": "/static/js/1.408e0e3a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "512d97871dfcc9088f263e317f4a5a6f",
+    "url": "/index.html"
+  }
+];
